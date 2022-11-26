35488d2d0b02573d0f55335f51589493fd8510b5
OleksandraO <oleksandra.orellana@gmail.com>
2022-11-25 19:35:12 -0500

    update the locate elements

--- google_search.py
+++ google_search.py
@@ -9,20 +9,20 @@ chrome_options = Options()
 chrome_options.add_experimental_option('detach', True)
 driver = webdriver.Chrome(options=chrome_options)
 # # driver = webdriver.Firefox()
-# print('maximizing the browser window')
-# driver.maximize_window()
-#
-# driver.get('http://www.google.com/')
-# time.sleep(5)
-#
-# search_box = driver.find_element(By.NAME, 'q')
-# search_box.send_keys('selenium')
-# search_box.send_keys(Keys.ENTER)
-# time.sleep(5)
-#
-# result = driver.find_element(By.ID, 'result-stats')
-# print(f'Search is done and result text is: {result.text}')
-#
-# print('closing the browser after test')
-# driver.quit()
-# print('Test completed')
+print('maximizing the browser window')
+driver.maximize_window()
+
+driver.get('http://www.google.com/')
+time.sleep(5)
+
+search_box = driver.find_element(By.NAME, 'q')
+search_box.send_keys('selenium')
+search_box.send_keys(Keys.ENTER)
+time.sleep(5)
+
+result = driver.find_element(By.ID, 'result-stats')
+print(f'Search is done and result text is: {result.text}')
+
+print('closing the browser after test')
+driver.quit()
+print('Test completed')
