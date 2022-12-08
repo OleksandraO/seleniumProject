e1cb5c6ab35ad031bd6065db7befbfcabed74f60
OleksandraO <oleksandra.orellana@gmail.com>
2022-12-07 21:42:18 -0500

    updated Explicit wait scenarios, test cases desc added

--- webelement_class/explicit_wait.py
+++ webelement_class/explicit_wait.py
@@ -4,7 +4,6 @@ from selenium import webdriver
 from selenium.common.exceptions import *
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from utilities import *
 
